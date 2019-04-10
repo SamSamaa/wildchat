3603b5e5194b8d27e53baf5fe57bbae20f6881ca
Julien Duhem <dhmjulien@gmail.com>
2019-04-10 16:57:15 +0200

    Added automatic scroll to bottom in feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UserMsg from '../UserMsg/UserMsg';
 import { List } from 'semantic-ui-react';
 import './Feed.css';
 
 function Feed() {
+
+  useEffect(() => {
+    document.getElementById('bottom').scrollIntoView({ behavior: 'smooth' });
+  });
+
   return (
     <List className="Feed">
       <List.Item><UserMsg /></List.Item>
@@ -27,6 +32,7 @@ function Feed() {
       <List.Item><UserMsg /></List.Item>
       <List.Item><UserMsg /></List.Item>
       <List.Item><UserMsg /></List.Item>
+      <div id='bottom'></div>
     </List>
   )
 }
