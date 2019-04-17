2034929f17145d6fd0ce9790bd7e532a4e8ba1d1
Bmontbe <bdemontbeillard@gmail.com>
2019-04-17 14:31:17 +0200

    add info user connection or disconnection

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -3,7 +3,7 @@ import UserMsg from './UserMsg/UserMsg';
 import { Client } from "../../Client";
 import { List } from 'semantic-ui-react';
 import './Feed.css';
-import SystemeMessage from "./SystemeMessage/SystemeMessage";
+// import SystemeMessage from "./SystemeMessage/SystemeMessage";
 
 
 function Feed() {
@@ -30,8 +30,15 @@ function Feed() {
     document.getElementById('bottom').scrollIntoView({ behavior: 'smooth' });
   });
 
+  const [systMsg, setSystMsg] = useState({});
+  
+  Client.receiveSystMsg((data => setSystMsg(data)));
+
   return (
     <List className="Feed">
+      <List.Item>
+        <div id="SystemeMessage"> {systMsg.user ? systMsg.user.name : null} {systMsg.statut ===0? " is disconnected" : null} {systMsg.statut ===1? " is connected" : null} </div>
+      </List.Item>
       {messages.map((message, index) => {
         return (
           <div>
@@ -42,7 +49,7 @@ function Feed() {
           </div>
         );
       })}
-        <SystemeMessage />
+         
       <div id='bottom'></div>
     </List>
   )
